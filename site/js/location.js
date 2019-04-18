452916d197e9e7563346595ae4b056d14c7f9e0d
Max Isom <code@maxisom.me>
2019-04-17 20:40:34 -0400

    Add full location

--- site/js/location.js
+++ site/js/location.js
@@ -16,7 +16,7 @@ $(document).ready(() => {
   const marker = new mapboxgl.Marker(mk)
                  .setLngLat([-88.546, 47.1179])
                  .setPopup(new mapboxgl.Popup({offset: 25})
-                    .setHTML('<h3>135 Fisher Hall</h3>'))
+                    .setHTML('<h3>135 Fisher Hall</h3><address><p>Michigan Technological University</p><p>1400 Townsend Drive</p><p>Houghton, Michigan 49931-1295</p></address>'))
                  .addTo(map);
 
   $('body').bind('light', () => {
